39f93ef0c49d9c5082d06f690b9025a39fbfc15f
Raphaël Candelier <raphael.candelier@sorbonne-universite.fr>
2023-01-19 12:16:46 +0100

    Switch style to furo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'Artificial Evolution Toolbox'
+project = 'AE Toolbox'
 copyright = 'Raphaël Candelier'
 author = 'Raphaël Candelier'
 
@@ -51,8 +51,9 @@ exclude_patterns = []
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
-html_theme = 'python_docs_theme'
+
+# html_theme = 'python_docs_theme'
+html_theme = 'furo'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
